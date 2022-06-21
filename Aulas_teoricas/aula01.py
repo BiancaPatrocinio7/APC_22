ddfa2435737cef41d150930ee038e8c61714e155
Bianca Patrocínio <70040539+BiancaPatrocinio7@users.noreply.github.com>
2022-06-21 09:38:49 -0300

    aula 01

---
+++ Aulas_teoricas/aula01.py
@@ -0,0 +1,15 @@
+#Variável
+#Uma variável refere-se a um espaço na memoria do seu computador, reservar para operções que vc da um apelido
+pi = 3.1416
+r = 4
+
+ano_nascimento = 2004
+cambio_dolar = 5.1416
+inflacao_anual = 0.05
+
+
+n = (2022 - ano_nascimento)
+c = 10
+a = 2*pi*r**2
+juros = c*(1+inflacao_anual)**n
+print(juros)
